68a0ff0ddba93fa7f782c132c5d088305c8711df
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-09 16:04:25 -0500

    more tweaks

--- src/socketServer/socketServer.js
+++ src/socketServer/socketServer.js
@@ -4,10 +4,10 @@ import path from 'path'
 
 import Facebook from './../facebook'
 
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-export function startServer() {
+export function startServer(server) {
 
 	const server = express()
 	  .use((req, res) => res.sendFile(INDEX) )
@@ -17,8 +17,7 @@ export function startServer() {
 
   	io.on('connection', (socket) => {
 	  	console.log('Client connected');
-
-      	Facebook(socket, 'connection');
+      	Facebook(socket, 'connection')
 
 	  	socket.on('disconnect', () => console.log('Client disconnected'));
 	});
