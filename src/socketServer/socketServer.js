cb86d421f9b8550fec12a9bdb78f0474191735e6
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-09 16:30:10 -0500

    fix

--- src/socketServer/socketServer.js
+++ src/socketServer/socketServer.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 import Facebook from './../facebook'
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8090;
 const INDEX = path.join(__dirname, 'index.html');
 
 export function startServer() {
