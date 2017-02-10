56668c0886e9e46b57a370a7d0984adb737ddf9e
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-10 11:47:14 -0500

    testing send

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.post('/webhook/', function (req, res) {
 })
 
 function sendTextMessage(sender, text) {
+    console.log(sender)
     let messageData = { text:text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
