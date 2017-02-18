fe1c95f8aa09f50adef794465d1a6cbd7eec32f5
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-18 03:05:27 -0500

    backtrack

--- index.js
+++ index.js
@@ -43,24 +43,11 @@ app.post('/webhook/', function (req, res) {
                 sendGenericMessage(sender)
                 continue
             }
-            getSenderName(sender, text.substring(0, 200))
-            // sendFacebookMessage(sender, "Text received from facebook gateway, echo: " + text.substring(0, 200))
+            receiveFacebookMessage(sender, text.substring(0, 200))
         }
     }
     res.sendStatus(200)
 })
-
-function getSenderName(sender, text) {
-    request
-        .post(`https://graph.facebook.com/v2.6/${sender}?fields=first_name,last_name&access_token=${token}`)
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-            if (!err) {
-                console.log(res)
-                receiveFacebookMessage(res.body, text)
-            } 
-        })
-}
 
 function receiveFacebookMessage(sender, text) {
     const newMessages = {
