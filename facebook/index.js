cca8f11182b499af73717198f37586282ea5ec06
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-18 17:27:32 -0500

    last try

--- facebook/index.js
+++ facebook/index.js
@@ -73,9 +73,10 @@ module.exports = {
             if (text === 'Generic') {
                 sendGenericMessage(sender)
                 continue
+            } else {
+            	sendFacebookMessage(req.body.sender_facebook_id, req.body.sender_facebook_id.text.substring(0, 200))
             }
-            sendFacebookMessage(req.body.sender_facebook_id, req.body.sender_facebook_id.text.substring(0, 200))
-
+            
 		    res.sendStatus(200)
 		})
 		function sendFacebookMessage(sender, text) {
